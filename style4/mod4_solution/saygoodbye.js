d02179200288d97009818151ece56ebc50ccb933
ZacSwayer <yadavpankaj07707@gmail.com>
2024-02-17 10:58:02 +0530

    new

--- style4/mod4_solution/saygoodbye.js
+++ style4/mod4_solution/saygoodbye.js
@@ -3,7 +3,7 @@
   var byeSpeaker = {};
   byeSpeaker.greeting = "Good Bye";
   byeSpeaker.Speak = function Speak(names) {
-  console.log(byeSpeaker.greeting + " " + (names));
+  console.log(byeSpeaker.greeting + " " + names);
 }
  window.byeSpeaker = byeSpeaker;
   })(window);
